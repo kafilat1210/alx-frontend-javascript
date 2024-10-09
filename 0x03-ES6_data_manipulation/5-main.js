b863f9abae0e4786404a5f5df9caa17171633965
kafilat1210 <cafylhearty@gmail.com>
2024-10-09 02:33:53 +0100

    Updated data manipulation

--- 0x03-ES6_data_manipulation/5-main.js
+++ 0x03-ES6_data_manipulation/5-main.js
@@ -1,3 +1,5 @@
 import createInt8TypedArray from './5-typed_array';
 
+console.log(createInt8TypedArray(10, 2, 89));import createInt8TypedArray from './5-typed_array';
+
 console.log(createInt8TypedArray(10, 2, 89));
